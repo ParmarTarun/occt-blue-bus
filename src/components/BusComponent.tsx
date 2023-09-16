9def907820402b41fc2ec047fe07aaa4978a9790
parmartarun <parmartarun.dev@gmail.com>
2023-09-16 13:08:10 -0400

    :sparkles: Make Bus Component collapseable

--- src/components/BusComponent.tsx
+++ src/components/BusComponent.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { busDataType } from "../types";
 import BusStop from "./BusStop";
 import NextStops from "./NextStops";
@@ -9,6 +10,8 @@ interface BusComponentProps {
 }
 
 const BusComponent = ({ busData, busTitle }: BusComponentProps) => {
+  const [expand, setExpand] = useState(false);
+
   const toggleTimings = (id: string) => {
     const el = document.querySelector<HTMLElement>("#" + id);
     if (el)
@@ -22,31 +25,52 @@ const BusComponent = ({ busData, busTitle }: BusComponentProps) => {
   };
 
   return (
-    <div className="m-2 p-2 text-primary">
-      <div className="border-b border-primary">
-        <h3 className="text-2xl">{busTitle.toUpperCase().replace("_", " ")}</h3>
+    <div className="m-2 p-2 text-primary min-w-98">
+      <div
+        className="flex items-center justify-between border-b border-primary w-56"
+        onClick={() => setExpand(!expand)}
+      >
+        <h3 className="text-2xl">
+          {busTitle.toUpperCase().replaceAll("_", " ")}
+        </h3>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-7 h-7"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+          />
+        </svg>
       </div>
 
-      <div className="flex flex-col justiify-between p-3">
-        {Object.entries(busData).map(([busStop, [timings, stops]], i) => (
-          <div key={i}>
-            <button
-              className=" p-2 bg-transparent border border-primary text-primary"
-              onClick={() => toggleTimings(`${busTitle}_${i}`)}
-            >
-              <BusStop title={busStop} />
-            </button>
-            <StopDetails
-              id={`${busTitle}_${i}`}
-              timings={timings}
-              busData={busData}
-            />
-            {Object.entries(busData).length - 1 !== i && ( // dont show next stops for last stop
-              <NextStops stops={stops} />
-            )}
-          </div>
-        ))}
-      </div>
+      {expand && (
+        <div className="flex flex-col p-3">
+          {Object.entries(busData).map(([busStop, [timings, stops]], i) => (
+            <div key={i}>
+              <button
+                className=" p-2 bg-transparent border border-primary text-primary"
+                onClick={() => toggleTimings(`${busTitle}_${i}`)}
+              >
+                <BusStop title={busStop} />
+              </button>
+              <StopDetails
+                id={`${busTitle}_${i}`}
+                timings={timings}
+                busData={busData}
+              />
+              {Object.entries(busData).length - 1 !== i && ( // dont show next stops for last stop
+                <NextStops stops={stops} />
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
