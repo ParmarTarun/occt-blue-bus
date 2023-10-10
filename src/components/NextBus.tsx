3ed1ecc97fd6f95415cb23b631770907ea5f2c7f
parmartarun <parmartarun.dev@gmail.com>
2023-10-10 16:00:58 -0400

    ✨ Make NextBus Collapseable

--- src/components/NextBus.tsx
+++ src/components/NextBus.tsx
@@ -6,12 +6,26 @@ import { nextBusesType } from "../types";
 import { findNextBuses } from "../utility";
 
 const NextBus = () => {
-  const [nextBuses, setNextBuses] = useState<nextBusesType>({});
+  const defaultNextBusCount = 3;
   const { routes } = useRoutes();
+  const totalNextBuses = Object.keys(routes).length;
+  const [nextBuses, setNextBuses] = useState<nextBusesType>({});
+  const [expanded, setExpanded] = useState(false);
+  const [nextBusCount, setNextBusCount] = useState(defaultNextBusCount);
 
   const handleStopSelect = (stop: string) => {
     const buses = findNextBuses(routes, stop);
     setNextBuses(buses);
+  };
+
+  const toggleExpand = () => {
+    if (expanded) {
+      setExpanded(false);
+      setNextBusCount(defaultNextBusCount);
+    } else {
+      setExpanded(true);
+      setNextBusCount(totalNextBuses);
+    }
   };
 
   useEffect(() => handleStopSelect(allstops[0]), [routes]);
@@ -36,23 +50,52 @@ const NextBus = () => {
         </div>
       </div>
       <div className="w-full grid lg:grid-cols-4 sm:grid-cols-1">
-        {Object.entries(nextBuses).map(
-          ([bus, times], i) =>
-            !!times.length && (
-              <div key={i} className="p-2 border border-primary rounded-md m-2">
-                <h3 className="text-primary mb-1">
-                  {bus.replaceAll("_", " ")}
-                </h3>
-                <div className="grid grid-cols-3">
-                  {times.slice(0, 3).map((time, i) => (
-                    <div className="mr-2" key={i}>
-                      <BusTime time={time} colorClass="text-green-800" />
-                    </div>
-                  ))}
+        {Object.entries(nextBuses)
+          .slice(0, nextBusCount)
+          .map(
+            ([bus, times], i) =>
+              !!times.length && (
+                <div
+                  key={i}
+                  className="p-2 border border-primary rounded-md m-2"
+                >
+                  <h3 className="text-primary mb-1">
+                    {bus.replaceAll("_", " ")}
+                  </h3>
+                  <div className="grid grid-cols-3">
+                    {times.slice(0, 3).map((time, i) => (
+                      <div className="mr-2" key={i}>
+                        <BusTime time={time} colorClass="text-green-800" />
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-            )
-        )}
+              )
+          )}
+        <p
+          className="text-primary flex justify-center items-center"
+          onClick={() => toggleExpand()}
+        >
+          {expanded
+            ? "collapse"
+            : totalNextBuses - defaultNextBusCount + " more"}
+          {!expanded && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+              />
+            </svg>
+          )}
+        </p>
       </div>
     </div>
   );
