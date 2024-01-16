34741e1911d1449426114e4b9bc4e399f3478ca3
parmartarun <parmartarun.dev@gmail.com>
2024-01-16 16:01:16 -0500

    ✨ Add Schedule Editor Table

--- src/components/ScheduleEditor.tsx
+++ src/components/ScheduleEditor.tsx
@@ -1,7 +1,118 @@
-import React from "react";
+import { useState } from "react";
+import { routesWeekly } from "../data/routesData";
+import ScheduleEditorTable from "./ScheduleEditorTable";
 
 const ScheduleEditor = () => {
-  return <div>ScheduleEditor</div>;
+  const [data, setData] = useState(routesWeekly);
+  const [openedBus, setOpenedBus] = useState("");
+  const [edited, setEdited] = useState(false);
+  const toggleSchedule = (bus: string) => {
+    // reset the current busData if opened tab is closed directly after editing
+    if (edited) {
+      resetData(openedBus);
+      setEdited(false);
+    }
+    if (openedBus === bus) {
+      setOpenedBus("");
+    } else {
+      setOpenedBus(bus);
+    }
+  };
+
+  const resetData = (bus: string) => {
+    setEdited(false);
+    setData({
+      ...data,
+      [bus]: routesWeekly[bus],
+    });
+  };
+
+  const saveData = () => {
+    alert("Make Api Call with data state");
+    setEdited(false);
+  };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    bus: string,
+    stop: string,
+    index: number
+  ) => {
+    // updates the specific bus time based on all the data
+    setEdited(true);
+    const [oldTimes, oldNextStops] = data[bus][stop];
+    const newTimes = [
+      ...oldTimes.slice(0, index),
+      e.target.value,
+      ...oldTimes.slice(index + 1),
+    ];
+    setData({
+      ...data,
+      [bus]: {
+        ...data[bus],
+        [stop]: [newTimes, [...oldNextStops]],
+      },
+    });
+  };
+
+  const addRow = (bus: string, i: number) => {
+    let newBusData = { ...data[bus] };
+    Object.keys(data[bus]).forEach((stop) => {
+      const [oldTimes, oldNextStops] = data[bus][stop];
+      newBusData[stop] = [
+        [...oldTimes.slice(0, i + 1), "00:00 AM", ...oldTimes.slice(i + 1)],
+        oldNextStops,
+      ];
+    });
+    setData({
+      ...data,
+      [bus]: newBusData,
+    });
+  };
+
+  return (
+    <div className="schedule-editor lg:w-1/2 lg:m-auto">
+      {Object.entries(data).map(([bus, busData], i) => {
+        return (
+          <section className="schedule-wrapper" key={i}>
+            <button
+              className="schedule-header w-full text-left"
+              onClick={() => toggleSchedule(bus)}
+            >
+              {bus}
+            </button>
+            <div className="h-1 bg-primary" />
+            <div
+              className={`schedule-table ${openedBus !== bus ? "hidden" : ""}`}
+            >
+              <ScheduleEditorTable
+                bus={bus}
+                handleChange={handleChange}
+                busData={busData}
+                addRow={addRow}
+              />
+              {edited && (
+                <div className="text-end p-2">
+                  <button
+                    className="bg-darkHighlight px-2 py-1 rounded-md text-primary mr-2"
+                    onClick={() => resetData(bus)}
+                  >
+                    RESET
+                  </button>
+                  <button
+                    className="bg-lightHighlight px-2 py-1 rounded-md text-primary"
+                    onClick={saveData}
+                  >
+                    SAVE
+                  </button>
+                </div>
+              )}
+            </div>
+          </section>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ScheduleEditor;
---
+++ src/components/ScheduleEditorTable.tsx
@@ -0,0 +1,88 @@
+import React, { FC } from "react";
+
+interface ScheduleEditorTableProps {
+  bus: string;
+  busData: { [key: string]: string[][] };
+  handleChange: (
+    e: React.ChangeEvent<HTMLInputElement>,
+    bus: string,
+    stop: string,
+    index: number
+  ) => void;
+  addRow: (bus: string, i: number) => void;
+}
+
+const ScheduleEditorTable: FC<ScheduleEditorTableProps> = ({
+  bus,
+  busData,
+  handleChange,
+  addRow,
+}) => {
+  const totalTimes = Object.values(busData)[0][0].length;
+  return (
+    <table className="w-full text-center">
+      <thead>
+        <tr>
+          {Object.keys(busData).map((stop) => (
+            <th key={stop}>{stop}</th>
+          ))}
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {Array(totalTimes)
+          .fill(0)
+          .map((_, j) => (
+            <tr key={j}>
+              {Object.entries(busData).map(([stop, data], k) => (
+                <td key={k}>
+                  <input
+                    type="text"
+                    value={data[0][j]}
+                    className=""
+                    onChange={(e) => handleChange(e, bus, stop, j)}
+                  />
+                </td>
+              ))}
+              <td className="flex gap-1">
+                <button onClick={() => addRow(bus, j)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                </button>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 text-red"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                    />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default ScheduleEditorTable;
